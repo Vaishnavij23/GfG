deeb505fd8dfa67cfb7f41dc8ce4d03b0abc60f6
Vaishnavi Joshi <114104350+Vaishnavij23@users.noreply.github.com>
2023-07-11 12:38:48 +0530

    Create sort1.java
    https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1?page=1&sortBy=submissions

---
+++ sort1.java
@@ -0,0 +1,10 @@
+/*Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.*/
+
+class Solution
+{
+    public static void sort012(int a[], int n)
+    {
+        Arrays.sort(a);
+    }
+}
+
